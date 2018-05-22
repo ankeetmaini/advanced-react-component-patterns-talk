cd414680529833f2d7643b89cb8458aa7bf80404
Ankeet Maini <ankeet.maini@gmail.com>
2018-05-22 15:29:22 +0530

    step-1: adds awesome back to top

--- src/feature-1.js
+++ src/feature-1.js
@@ -5,7 +5,39 @@ const longListOfDivs = Array.from({ length: 500 }).map((l, index) => (
 ));
 
 export default class Feature1 extends React.Component {
+  state = { showBackToTop: false };
+
+  componentDidMount() {
+    this.observer = new IntersectionObserver(
+      entries => {
+        entries.forEach(entry => {
+          this.setState({ showBackToTop: !entry.intersectionRatio });
+        });
+      },
+      {
+        root: null,
+        threshold: 0
+      }
+    );
+    this.observer.observe(document.getElementById("sentinel"));
+  }
+
+  componentWillUnmount() {
+    this.observer &&
+      this.observer.unobserve(document.getElementById("sentinel"));
+  }
+
   render() {
-    return <div>{longListOfDivs}</div>;
+    return (
+      <div>
+        <div id="sentinel" />
+        {longListOfDivs}
+        {this.state.showBackToTop && (
+          <button className="backToTop" onClick={() => window.scrollTo(0, 0)}>
+            Back To Top
+          </button>
+        )}
+      </div>
+    );
   }
 }
